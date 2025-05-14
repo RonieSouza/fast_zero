88ee6798814c4231090c032d762eecc94e8eff38
Ronie Souza <roniesilvamarinhosouza@gmail.com>
2025-05-13 23:50:48 -0300

    Criando repositório e adicionando schema de mensagem Aula 1 e 2

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+
+def test_exercicio_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/exercicio-html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
